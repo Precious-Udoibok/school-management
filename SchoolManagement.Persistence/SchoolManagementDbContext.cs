903eedfa93d9ef8c704fbcd4be3d78f1614c78bd
Precious Udoibok <preciousudoibok@gmail.com>
2025-08-17 04:47:52 +0100

    Setup the database

---
+++ SchoolManagement.Persistence/SchoolManagementDbContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace SchoolManagement.Persistence
+{
+    internal class SchoolManagementDbContext : DbContext
+    {
+        public SchoolManagementDbContext(DbContextOptions<SchoolManagementDbContext>options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DataSet<>
+    }
+}
